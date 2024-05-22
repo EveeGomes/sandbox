b1ded328e03e6b9f95d9c2a4464959d79dec9e83
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-05-22 11:26:33 -0300

    Implement an observer of Person interface

--- Design-Patterns/Observer/observer-pattern/Main.cpp
+++ Design-Patterns/Observer/observer-pattern/Main.cpp
@@ -3,6 +3,9 @@
 // Observer: the thing that monitors something
 // Observable: the component is the one to be obeserved
 
+#include <iostream>
+#include "Observer.h"
+
 class Person // observable
 {
    int m_age;
@@ -19,6 +22,23 @@ public:
       m_age = age; // in the video he uses Person::m_age = age;
    }
 };
+
+// Make a simple observer that prints something to the console. 
+// It'll implement an Observer of Person interface
+struct ConsolePersonObserver : public Observer<Person>
+{
+private:
+   // implement the field_changed virtual method
+   void field_changed(Person& source, const std::string& field_name) override
+   {
+      std::cout << "Person's " << field_name << " has changed to: ";
+      if (field_name == "age") std::cout << source.get_age();
+      std::cout << ".\n";
+   }
+};
+
+// However to use that observer the Observable class needs to support this paradigm 
+
 
 int main(int arg, char* args[])
 {
