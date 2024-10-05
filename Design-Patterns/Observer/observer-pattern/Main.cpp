83cc949a258b043c4f0abea6c8ea0d9699ee7629
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-10-05 10:01:03 -0300

    L106. adding notification to another field

--- Design-Patterns/Observer/observer-pattern/Main.cpp
+++ Design-Patterns/Observer/observer-pattern/Main.cpp
@@ -25,21 +25,35 @@ public:
    {
       // Now, Person has the ability to not only change the age but inform somebody about this change!
 
-      // 1st check if the change has indeed made:
+      // 1st check if the change has indeed been made (i.e. check for self assignment):
       if (m_age == age) return;
+
+      // Now, before setting the age, we cache the old can_vote value in order to see if it has in fact changed or not.
+      auto old_can_vote = get_can_vote();
 
       // set the value and notify:
       m_age = age;
       notify(*this, "age");
+
+      // Compare the old value with the new value to determine if we'd need notification on can_vote as well.
+      if (old_can_vote != get_can_vote())
+      {
+         // notify on this object and the name of the property (or in this case the "field" we have through the getter method get_can_vote)
+         notify(*this, "can_vote");
+      }
    }
 
    /** 
    * There's an issue with this paradigm which lies in dependencies. (Lesson 106)
    * Say we introduce a flag that tells whether a person can vote: get_can_vote(). If we want to manage the changes of that flag, we don't have
    *  a way of calling notify on it, because there is no setter (and so it becomes unclear where we should call notify).
-   * The notify should be called in set_age since as we change the age, the flag also get affected!
-   * 
+   * The notify should be called in set_age since as we change the age, the flag also gets affected!
    */
+
+   bool get_can_vote() const
+   {
+      return m_age >= 16;
+   }
 };
 
 // Make a simple observer that prints something to the console. 
