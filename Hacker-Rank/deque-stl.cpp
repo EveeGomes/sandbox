f358209b23cf5cad0eb82fc404434d5797b283f4
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-11-07 18:36:59 -0300

    Add submitted solution to deque-stl with comments about each operation using the Sliding Window concept

--- Hacker-Rank/deque-stl.cpp
+++ Hacker-Rank/deque-stl.cpp
@@ -1,50 +1,73 @@
 #include <iostream>
 #include <deque>
+#include <vector>
 
 
 int main()
 {
+   // Number of tests coming from "user"/test
+   int T{};
+   std::cin >> T;
 
-   std::deque<std::deque<int>> B;
-   //int length = 7;
-
-   // size of the inner deques
-   int l = 2;
-
-   // Add empty subdeques before adding elements to these subdeques (inner deque):
-   for (int i = 0; i < l; i++)
+   while (T > 0) // or T-- ?
    {
-      // add empty inner deque to B
-      B.push_back(std::deque<int>());
-   }
+      // window to slide through numbers. It holds the indices of elements in numbers vector
+      std::deque<int> window{};
 
-   //int first[7] = { 3, 4, 5, 8, 1, 4, 10 };
-   int first[5] = { 3, 4, 6, 3, 4};
+      // Largests ints in each window
+      std::vector<int> largests{};
 
-   // Now, populate the inner deques
-   int indexB = 0;
+      int lengthNumbers{};
+      int windowLength{};
 
-   for (int i = 0; i < l; i++)
-   {
-      indexB = i;
+      // Get lengths of numbers and window
+      std::cin >> lengthNumbers >> windowLength;
 
-      for (int j = 0; j < l; j++)
+      // Array of numbers coming from "user"/test
+      std::vector<int> numbers(lengthNumbers);
+      // Populate numbers
+      for (int i = 0; i < lengthNumbers; ++i)
       {
-         B[i].push_back(first[indexB]);
-         indexB++;
+         std::cin >> numbers[i];
       }
 
-      //indexB++;
-   }
-
-
-   for (int i = 0; i < B.size(); i++)
-   {
-      for (int j = 0; j < B[i].size(); j++)
+      // Process each element in numbers
+      for (int i = 0; i < lengthNumbers; ++i)
       {
-         std::cout << B[i][j] << " ";
+         // Remove elements out of the current window
+         if (!window.empty() && window.front() <= i - windowLength)
+         {
+            // Remove the first element if it's lower than the current index minus this window's length (it's older), remove it.
+            //  That ensures that the window only has indices of elements within the current window.
+            window.pop_front();
+         }
+
+         // Remove index (window element) from the window that represents a smaller element than the current index represents in numbers!
+         while (!window.empty() && numbers[window.back()] <= numbers[i])
+         {
+            window.pop_back();
+         }
+
+         // Add the current element (current index) to the back of the window
+         window.push_back(i);
+
+         // For the current window (as soon as a full window is achieved: the current index is >= windowLength - 1), the element at the front
+         //  represents the index of the element in numbers that's the largest one!
+         if (i >= windowLength - 1) // means the window is filled, therefore we're in the current window
+         {
+            // Then, add the numbers element to the largest array using the index at the front in the window
+            largests.push_back(numbers[window.front()]);
+         }
+      }
+
+      // Now, print the maximum/largest values
+      for (int& largest : largests)
+      {
+         std::cout << largest << " ";
       }
       std::cout << std::endl;
+
+      T--; // if we use while (T--), this isn't needed
    }
 
    return 0;
