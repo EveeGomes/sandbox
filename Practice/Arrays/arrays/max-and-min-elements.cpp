ea8206d23580cd3de2e0b94febd5174cf8b60cfd
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-11-18 19:56:12 -0300

    Max and Min elements

---
+++ Practice/Arrays/arrays/max-and-min-elements.cpp
@@ -0,0 +1,6 @@
+﻿Build started at 7:51 PM...
+1>------ Build started: Project: arrays, Configuration: Debug x64 ------
+1>main.cpp
+1>arrays.vcxproj -> C:\Users\evepg\Documents\VS-Sandbox\Practice\Arrays\arrays\x64\Debug\arrays.exe
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
+========== Build completed at 7:51 PM and took 00.995 seconds ==========
