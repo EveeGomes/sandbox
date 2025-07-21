ee7e04035e8477c980b78b6a14defd017e571b7a
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2025-07-21 10:01:12 -0300

    Add some comments explaining the design pattern

--- Design-Patterns/Command/command-pattern/main.cpp
+++ Design-Patterns/Command/command-pattern/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 #include <vector>
 
+/*
+* The command pattern allows for decoupling the command from the real action.
+* There should be a separation of concerns where there's a command class, serving as a base class which is an interface with a single pure virtual method,
+*  which forces any derived class to implement it. Normally it's called "Execute()".
+* In a derived class, which is a concrete command, there will be a reference to a receiver as well as any data/info that's needed to implement the Execute
+*  method. In that implementation we'll use the receiver reference to call a method responsible for the action that it'll be performed by this receiver.
+*  So in this scenario, the command per se has no clue how the action really works, it only issues the command through the receiver reference.
+* Then, we can have a handler (or invoker) to call the Execute() method according to some input, for example. Say we're talking about implementing an input
+*  handler, where buttons are bound to certain commands, so when a player presses space it will issue the JumpCommand which will tell the character to perform
+*  its jump method. That will happen as:
+*  Command* space = JumpCommand(&player);
+*  space->Execute();
+*/
+
+// Receiver
 struct BankAccount
 {
    int balance{ 0 };
@@ -35,11 +50,13 @@ struct BankAccount
 * For it we'll have objects, that we'll call commands, for performing those operations.
 */
 
+// Command interface
 struct Command
 {
    virtual void call() = 0;
 };
 
+// Concrete command
 struct BankAccountCommand : Command
 {
    // Have a reference to the bank account we'll operate on
@@ -79,10 +96,10 @@ int main()
       BankAccountCommand{ba, BankAccountCommand::deposit, 100},
       BankAccountCommand{ba, BankAccountCommand::withdraw, 200}
    };
-
    std::cout << ba << std::endl;
 
    /*
+   * Invoker
    * Process the commands: each command calls call() and perform what the action they have reference of
    */
    for (auto& cmd : commands)
