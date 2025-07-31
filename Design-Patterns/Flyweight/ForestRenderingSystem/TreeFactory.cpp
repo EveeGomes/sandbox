b1c87d7de2c0b05838b6d3766408b36f75753c80
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2025-07-31 20:04:16 -0300

    Refactor GetFreeType

--- Design-Patterns/Flyweight/ForestRenderingSystem/TreeFactory.cpp
+++ Design-Patterns/Flyweight/ForestRenderingSystem/TreeFactory.cpp
@@ -2,28 +2,13 @@
 
 TreeType* TreeFactory::GetTreeType(std::string name, std::string texture, std::string mesh)
 {
-   std::map<std::string, TreeType*>::iterator treeIterator;
-   treeIterator = _treeTypeInstances.find(name);
-
-   //if (treeIterator != _treeTypeInstances.end())
-   //{
-   //   // it already exists, return it
-   //   return treeIterator->second;
-   //}
-   //else
-   //{
-   //   // create one, store in the map and return it
-   //}
+   auto treeIterator = _treeTypeInstances.find(name);
 
    if (treeIterator == _treeTypeInstances.end())
    {
       // it doesn't exist, so create one, store in the map and return it
-      TreeType* newTreePtr = new TreeType{ name, texture, mesh };
-      _treeTypeInstances.insert(std::pair<std::string, TreeType*>(name, newTreePtr));
-      treeIterator = _treeTypeInstances.find(name);
-
-      return treeIterator->second;
+      _treeTypeInstances[name] = new TreeType{ name, texture, mesh };
    }
    
-   return treeIterator->second;
+   return _treeTypeInstances[name];
 }
