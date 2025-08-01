070dcf9d367d467eef58f00853ff37bafcaf0e2d
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2025-07-31 22:10:13 -0300

    Change raw pointer for shared_ptr

--- Design-Patterns/Flyweight/ForestRenderingSystem/TreeFactory.cpp
+++ Design-Patterns/Flyweight/ForestRenderingSystem/TreeFactory.cpp
@@ -1,13 +1,13 @@
 #include "TreeFactory.h"
 
-TreeType* TreeFactory::GetTreeType(std::string name, std::string texture, std::string mesh)
+std::shared_ptr<TreeType> TreeFactory::GetTreeType(std::string name, std::string texture, std::string mesh)
 {
    auto treeIterator = _treeTypeInstances.find(name);
 
    if (treeIterator == _treeTypeInstances.end())
    {
       // it doesn't exist, so create one, store in the map and return it
-      _treeTypeInstances[name] = new TreeType{ name, texture, mesh };
+      _treeTypeInstances[name] = std::make_shared<TreeType>(name, texture, mesh );
    }
    
    return _treeTypeInstances[name];
--- Design-Patterns/Flyweight/ForestRenderingSystem/TreeFactory.h
+++ Design-Patterns/Flyweight/ForestRenderingSystem/TreeFactory.h
@@ -2,17 +2,18 @@
 #define TREEFACTORY_H
 
 #include <map>
+#include <memory>
 #include "TreeType.h"
 
 class TreeFactory
 {
-   std::map<std::string, TreeType*> _treeTypeInstances;
+   std::map<std::string, std::shared_ptr<TreeType>> _treeTypeInstances;
 
 public:
    TreeFactory() {};
 
    // Returns a TreeType if there's any, otherwise create, store and return one
-   TreeType* GetTreeType(std::string name, std::string texture, std::string mesh);
+   std::shared_ptr<TreeType> GetTreeType(std::string name, std::string texture, std::string mesh);
 };
 
 #endif // !TREEFACTORY_H
